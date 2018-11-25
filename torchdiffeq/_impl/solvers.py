b3d155ecb682ae6a54dd973ba62cc7d4c63fcd30
Ricky Chen <rtqichen@gmail.com>
2018-11-25 15:13:53 -0500

    rm warnings for fixed grid

--- torchdiffeq/_impl/solvers.py
+++ torchdiffeq/_impl/solvers.py
@@ -37,6 +37,8 @@ class FixedGridODESolver(object):
     __metaclass__ = abc.ABCMeta
 
     def __init__(self, func, y0, step_size=None, grid_constructor=None, **unused_kwargs):
+        unused_kwargs.pop('rtol', None)
+        unused_kwargs.pop('atol', None)
         _handle_unused_kwargs(self, unused_kwargs)
         del unused_kwargs
 
