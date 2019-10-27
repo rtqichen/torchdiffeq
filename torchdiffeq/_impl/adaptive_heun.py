54a5013f4e9cd714fa5db173ba69dfd05514da4b
timudk <timdockhorn@gmail.com>
2019-10-27 13:52:07 -0400

    Fixing C_mid and tests

--- torchdiffeq/_impl/adaptive_heun.py
+++ torchdiffeq/_impl/adaptive_heun.py
@@ -21,7 +21,7 @@ _ADAPTIVE_HEUN_TABLEAU = _ButcherTableau(
 )
 
 AH_C_MID = [
-    1., 0.
+    0.5, 0.
 ]
 
 
@@ -76,7 +76,7 @@ class AdaptiveHeunSolver(AdaptiveStepsizeODESolver):
         n_steps = 0
         while next_t > self.rk_state.t1:
             assert n_steps < self.max_num_steps, 'max_num_steps exceeded ({}>={})'.format(n_steps, self.max_num_steps)
-            self.rk_state = self._adaptive_dopri5_step(self.rk_state)
+            self.rk_state = self._adaptive_heun_step(self.rk_state)
             n_steps += 1
         return _interp_evaluate(self.rk_state.interp_coeff, self.rk_state.t0, self.rk_state.t1, next_t)
 
@@ -89,7 +89,7 @@ class AdaptiveHeunSolver(AdaptiveStepsizeODESolver):
         assert t0 + dt > t0, 'underflow in dt {}'.format(dt.item())
         for y0_ in y0:
             assert _is_finite(torch.abs(y0_)), 'non-finite values in state `y`: {}'.format(y0_)
-        y1, f1, y1_error, k = _runge_kutta_step(self.func, y0, f0, t0, dt, tableau=_DORMAND_PRINCE_SHAMPINE_TABLEAU)
+        y1, f1, y1_error, k = _runge_kutta_step(self.func, y0, f0, t0, dt, tableau=_ADAPTIVE_HEUN_TABLEAU)
 
         ########################################################
         #                     Error Ratio                      #
