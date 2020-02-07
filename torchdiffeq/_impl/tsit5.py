f312f1f53d4798fd1cbd31e4121fa39d2ecebca7
Chris Finlay <christopher.finlay@gmail.com>
2020-02-07 16:11:47 -0500

    Bogacki-Shampine solver (MATLAB's ode23), and first_step bug fix

--- torchdiffeq/_impl/tsit5.py
+++ torchdiffeq/_impl/tsit5.py
@@ -86,7 +86,7 @@ class Tsit5Solver(AdaptiveStepsizeODESolver):
         if self.first_step is None:
             first_step = _select_initial_step(self.func, t[0], self.y0, 4, self.rtol, self.atol).to(t)
         else:
-            first_step = _convert_to_tensor(0.01, dtype=t.dtype, device=t.device)
+            first_step = _convert_to_tensor(self.first_step, dtype=t.dtype, device=t.device)
         self.rk_state = _RungeKuttaState(
             self.y0,
             self.func(t[0].type_as(self.y0[0]), self.y0), t[0], t[0], first_step,
