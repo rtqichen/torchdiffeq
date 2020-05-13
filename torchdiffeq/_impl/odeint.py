55fa6af17afc51c728c67b856433d49143229393
Ricky Chen <rtqichen@gmail.com>
2020-05-13 12:21:50 -0400

    Update odeint.py

--- torchdiffeq/_impl/odeint.py
+++ torchdiffeq/_impl/odeint.py
@@ -28,19 +28,19 @@ def odeint(func, y0, t, rtol=1e-7, atol=1e-9, method=None, options=None):
         ```
         dy/dt = func(t, y), y(t[0]) = y0
         ```
-    where y is a Tensor of any shape.
+    where y is a Tensor or tuple of Tensors of any shape.
 
     Output dtypes and numerical precision are based on the dtypes of the inputs `y0`.
 
     Args:
-        func: Function that maps a Tensor holding the state `y` and a scalar Tensor
-            `t` into a Tensor of state derivatives with respect to time.
-        y0: N-D Tensor giving starting value of `y` at time point `t[0]`. May
-            have any floating point or complex dtype.
+        func: Function that maps a scalar Tensor `t` and a Tensor holding the state `y`
+            into a Tensor of state derivatives with respect to time. Optionally, `y`
+            can also be a tuple of Tensors.
+        y0: N-D Tensor giving starting value of `y` at time point `t[0]`. Optionally, `y0`
+            can also be a tuple of Tensors.
         t: 1-D Tensor holding a sequence of time points for which to solve for
             `y`. The initial time point should be the first element of this sequence,
-            and each time must be larger than the previous time. May have any floating
-            point dtype. Converted to a Tensor with float64 dtype.
+            and each time must be larger than the previous time. 
         rtol: optional float64 Tensor specifying an upper bound on relative error,
             per element of `y`.
         atol: optional float64 Tensor specifying an upper bound on absolute error,
@@ -48,7 +48,6 @@ def odeint(func, y0, t, rtol=1e-7, atol=1e-9, method=None, options=None):
         method: optional string indicating the integration method to use.
         options: optional dict of configuring options for the indicated integration
             method. Can only be provided if a `method` is explicitly set.
-        name: Optional name for this operation.
 
     Returns:
         y: Tensor, where the first dimension corresponds to different
@@ -58,8 +57,6 @@ def odeint(func, y0, t, rtol=1e-7, atol=1e-9, method=None, options=None):
 
     Raises:
         ValueError: if an invalid `method` is provided.
-        TypeError: if `options` is supplied without `method`, or if `t` or `y0` has
-            an invalid dtype.
     """
 
     tensor_input, func, y0, t = _check_inputs(func, y0, t)
@@ -68,9 +65,13 @@ def odeint(func, y0, t, rtol=1e-7, atol=1e-9, method=None, options=None):
         options = {}
     elif method is None:
         raise ValueError('cannot supply `options` without specifying `method`')
-
+        
     if method is None:
         method = 'dopri5'
+        
+    if method not in SOLVERS:
+        raise ValueError('Invalid method "{}". Must be one of {}'.format(
+                         method, '{"' + '", "'.join(SOLVERS.keys()) + '"}.'))
 
     solver = SOLVERS[method](func, y0, rtol=rtol, atol=atol, **options)
     solution = solver.integrate(t)
