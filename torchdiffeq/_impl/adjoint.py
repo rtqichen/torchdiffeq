9dda0aa46ea7ca064e20e0b3d5c261a359065a84
Ricky Chen <rtqichen@fb.com>
2020-08-12 13:41:32 -0700

    fix case where adjoint_params are from a generator

--- torchdiffeq/_impl/adjoint.py
+++ torchdiffeq/_impl/adjoint.py
@@ -172,6 +172,8 @@ def odeint_adjoint(func, y0, t, rtol=1e-7, atol=1e-9, method=None, options=None,
         adjoint_options = {k: v for k, v in options.items() if k != "norm"} if options is not None else {}
     if adjoint_params is None:
         adjoint_params = tuple(func.parameters())
+    else:
+        adjoint_params = tuple(adjoint_params)  # in case adjoint_params is a generator.
 
     # Normalise to non-tupled input
     shapes, func, y0, t, rtol, atol, method, options = _check_inputs(func, y0, t, rtol, atol, method, options, SOLVERS)
