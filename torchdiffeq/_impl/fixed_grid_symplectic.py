e06f984374b84a948ac7ba4ed9e295b61d0b6b5f
Fumihiro Ishikawa <fumihiro@ai.phys.s.u-tokyo.ac.jp>
2020-09-21 22:05:38 +0900

    modify stupid mistakes

--- torchdiffeq/_impl/fixed_grid_symplectic.py
+++ torchdiffeq/_impl/fixed_grid_symplectic.py
@@ -19,28 +19,24 @@ class Yoshida4th(FixedGridODESolver):
         self.eps = torch.as_tensor(eps, dtype=self.dtype, device=self.device)
 
     def _step_symplectic(self, func, y, t, h, h2):
-        dy = torch.zeros(y.size())
+        dy = torch.zeros(y.size(),dtype=self.dtype,device=self.device)
         n = len(y) // 2
         k_ = func(t + self.eps, y)
-        for i in range(n):
-            dy[i] = h*_c1*k_[i] + h2*_c1*_b1*k_[n+i]
-            dy[n+i] = h*_b1*k_[n+i]
+        dy[:n] = h*_c1*k_[:n] + h2*_c1*_b1*k_[n:]
+        dy[n:] = h*_b1*k_[n:]
 
         k_ = func(t + self.eps, y + dy)
-        for i in range(n):
-            dy[i] = dy[i] \
-                         + h*_c2*k_[i] + h2*_c2*_b2*k_[n+i]
-            dy[n+i] = dy[n+i] + h*_b2*k_[n+i]
+        dy[:n] = dy[:n] \
+                + h*_c2*k_[:n] + h2*_c2*_b2*k_[n:]
+        dy[n:] = dy[n:] + h*_b2*k_[n:]
 
         k_ = func(t + self.eps, y + dy)
-        for i in range(n):
-            dy[i] = dy[i] \
-                         + h*_c1*k_[i] + h2*_c1*_b2*k_[n+i]
-            dy[n+i] = dy[n+i] + h*_b2*k_[n+i]
+        dy[:n] = dy[:n] \
+                + h*_c1*k_[:n] + h2*_c1*_b2*k_[n:]
+        dy[n:] = dy[n:] + h*_b2*k_[n:]
 
         k_ = func(t + self.eps, y + dy)
-        for i in range(n):
-            dy[n+i] = dy[n+i] + h*_b1*k_[n+i]
+        dy[n:] = dy[n:] + h*_b1*k_[n:]
 
         return dy
 
