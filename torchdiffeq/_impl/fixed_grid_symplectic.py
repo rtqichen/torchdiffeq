90206995d13849eca2417f183b1d6082e26d5d5b
Fumihiro Ishikawa <fumihiro@ai.phys.s.u-tokyo.ac.jp>
2020-09-21 20:59:05 +0900

    change variable ordering

--- torchdiffeq/_impl/fixed_grid_symplectic.py
+++ torchdiffeq/_impl/fixed_grid_symplectic.py
@@ -18,46 +18,35 @@ class Yoshida4th(FixedGridODESolver):
         super(Yoshida4th, self).__init__(**kwargs)
         self.eps = torch.as_tensor(eps, dtype=self.dtype, device=self.device)
 
-    def _step_symplectic(self, func, y, dy, t, h, h2, bs, n):
+    def _step_symplectic(self, func, y, t, h, h2):
+        dy = torch.zeros(y.size())
+        n = len(y) // 2
         k_ = func(t + self.eps, y)
-        for i in range(bs):
-            for j in range(n):
-                dy[2*n*i+j] = h*_c1*k_[2*n*i+j] + h2*_c1*_b1*k_[2*n*i+n+j]
-                dy[2*n*i+n+j] = h*_b1*k_[2*n*i+n+j]
+        for i in range(n):
+            dy[i] = h*_c1*k_[i] + h2*_c1*_b1*k_[n+i]
+            dy[n+i] = h*_b1*k_[n+i]
 
         k_ = func(t + self.eps, y + dy)
-        for i in range(bs):
-            for j in range(n):
-                dy[2*n*i+j] = dy[2*n*i+j] \
-                             + h*_c2*k_[2*n*i+j] + h2*_c2*_b2*k_[2*n*i+n+j]
-                dy[2*n*i+n+j] = dy[2*n*i+n+j] + h*_b2*k_[2*n*i+n+j]
+        for i in range(n):
+            dy[i] = dy[i] \
+                         + h*_c2*k_[i] + h2*_c2*_b2*k_[n+i]
+            dy[n+i] = dy[n+i] + h*_b2*k_[n+i]
 
         k_ = func(t + self.eps, y + dy)
-        for i in range(bs):
-            for j in range(n):
-                dy[2*n*i+j] = dy[2*n*i+j] \
-                             + h*_c1*k_[2*n*i+j] + h2*_c1*_b2*k_[2*n*i+n+j]
-                dy[2*n*i+n+j] = dy[2*n*i+n+j] + h*_b2*k_[2*n*i+n+j]
+        for i in range(n):
+            dy[i] = dy[i] \
+                         + h*_c1*k_[i] + h2*_c1*_b2*k_[n+i]
+            dy[n+i] = dy[n+i] + h*_b2*k_[n+i]
 
         k_ = func(t + self.eps, y + dy)
-        for i in range(bs):
-            for j in range(n):
-                dy[2*n*i+n+j] = dy[2*n*i+n+j] + h*_b1*k_[2*n*i+n+j]
+        for i in range(n):
+            dy[n+i] = dy[n+i] + h*_b1*k_[n+i]
 
         return dy
 
     def _step_func(self, func, t, dt, y):
         h2 = dt * dt
         h = dt
-        dy = torch.zeros(y.size())
 
-        if len(func.shapes) < 3:
-            config_shape= func.shapes[0]
-            bs = config_shape[0]
-            n = config_shape[1] // 2
-            return self._step_symplectic(func, y, dy, t, h, h2, bs, n)
-        else:
-            return rk4_alt_step_func(func, t + self.eps, dt - 2 * self.eps, y)
-
-
+        return self._step_symplectic(func, y, t, h, h2)
 
