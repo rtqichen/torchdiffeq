b0691e44c573336d297aac8db2b1ebb9285f9b92
Fumihiro Ishikawa <fumihiro@ai.phys.s.u-tokyo.ac.jp>
2020-09-23 17:19:55 +0900

    added symplectic

--- tests/gradient_tests.py
+++ tests/gradient_tests.py
@@ -2,7 +2,8 @@ import unittest
 import torch
 import torchdiffeq
 
-from problems import construct_problem, PROBLEMS, DEVICES, METHODS
+from problems import construct_problem, PROBLEMS, DEVICES, METHODS, \
+                     FIXED_SYMPLECTIC_METHODS
 
 
 def max_abs(tensor):
@@ -15,7 +16,10 @@ class TestGradient(unittest.TestCase):
             for method in METHODS:
 
                 with self.subTest(device=device, method=method):
-                    f, y0, t_points, _ = construct_problem(device=device)
+                    if method in FIXED_SYMPLECTIC_METHODS:
+                        f, y0, t_points, _ = construct_problem(device=device,ode='constant_symplectic')
+                    else:
+                        f, y0, t_points, _ = construct_problem(device=device)
                     func = lambda y0, t_points: torchdiffeq.odeint(f, y0, t_points, method=method)
                     self.assertTrue(torch.autograd.gradcheck(func, (y0, t_points)))
 
@@ -32,15 +36,17 @@ class TestGradient(unittest.TestCase):
                         eps = 1e-5
                     elif ode == 'sine':
                         eps = 5e-3
+                    elif ode in ['harmonic','constant_symplectic']:
+                        eps = 1e-5
                     else:
                         raise RuntimeError
 
                     with self.subTest(device=device, ode=ode, t_grad=t_grad):
+                        torch.manual_seed(1234)
                         f, y0, t_points, _ = construct_problem(device=device, ode=ode)
                         t_points.requires_grad_(t_grad)
 
                         ys = torchdiffeq.odeint(f, y0, t_points, rtol=1e-9, atol=1e-12)
-                        torch.manual_seed(0)
                         gradys = torch.rand_like(ys)
                         ys.backward(gradys)
 
@@ -116,6 +122,52 @@ class TestCompareAdjointGradient(unittest.TestCase):
                         if t_grad:
                             self.assertLess(max_abs(t_points.grad - adj_t_grad), eps[1])
                         self.assertLess(max_abs(func.A.grad - adj_A_grad), eps[2])
+
+
+class TestCompareAdjointGradientforSymplectic(unittest.TestCase):
+    def test_adjoint_sympelctic(self):
+
+        for device in DEVICES:
+            for ode in ['harmonic','constant_symplectic']:
+                eps = 3*1e-4
+                dt = 0.01
+                for t_grad in (True,False):
+                    torch.manual_seed(1234)
+                    with self.subTest(device=device, ode=ode, t_grad=t_grad):
+                        f, y0, t_points, _ = construct_problem(device=device, ode=ode)
+                        t_points.requires_grad_(t_grad)
+
+                        ys = torchdiffeq.odeint(f, y0, t_points, method='dopri5')
+                        gradys = torch.rand_like(ys)
+                        ys.backward(gradys)
+
+                        reg_y0_grad = y0.grad.clone()
+                        reg_t_grad = t_points.grad.clone() if t_grad else None
+                        reg_params_grads = []
+                        for param in f.parameters():
+                            reg_params_grads.append(param.grad.clone())
+
+                        y0.grad.zero_()
+                        if t_grad:
+                            t_points.grad.zero_()
+                        for param in f.parameters():
+                            param.grad.zero_()
+                        ys = torchdiffeq.odeint_adjoint(f, y0, t_points, 
+                                                     method='yoshida4th',
+                                                     options={'step_size':dt})
+                        ys.backward(gradys)
+
+                        adj_y0_grad = y0.grad
+                        adj_t_grad = t_points.grad if t_grad else None
+                        adj_params_grads = []
+                        for param in f.parameters():
+                            adj_params_grads.append(param.grad)
+
+                        self.assertLess(max_abs(reg_y0_grad - adj_y0_grad), eps)
+                        if t_grad:
+                            self.assertLess(max_abs(reg_t_grad - adj_t_grad), eps)
+                        for reg_grad, adj_grad in zip(reg_params_grads, adj_params_grads):
+                            self.assertLess(max_abs(reg_grad - adj_grad), eps)
 
 
 if __name__ == '__main__':
