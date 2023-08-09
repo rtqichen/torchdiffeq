0fe424b9859ccf3a597a335d73f8f770ae518e3d
Eleni Michaelidou <s2226022@ed.ac.uk>
2023-08-09 21:44:15 +0100

    low percentages

--- examples/experiments.py
+++ examples/experiments.py
@@ -5,14 +5,15 @@ import subprocess
 # List of different arguments to try
 networks = ['odenet', 'resnet']
 batch_sizes = [128]
-data_percentages = [0.25, 0.50, 0.75, 1.0]  # List of data percentages
-
+#data_percentages = [0.25, 0.50, 0.75, 1.0]  # List of data percentages
+data_percentages = [0.01, 0.05, 0.10, 0.15, 0.20]
 
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 venv_path = os.path.join(parent_dir, 'myenv', 'bin', 'activate')
 
-script_filenames = ['odenet_mnist.py', 'odenet_cifar10.py']
+#script_filenames = ['odenet_mnist.py', 'odenet_cifar10.py']
+script_filenames = ['odenet_mnist.py']
 
 for script_filename in script_filenames:
     for network, batch_size, data_percentage in itertools.product(networks, batch_sizes, data_percentages):
