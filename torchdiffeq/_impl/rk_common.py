de4c8eb8f38d294fc748e2689ab200ddf54229e4
pollycoder <pollyjoe2003@gmail.com>
2025-02-13 19:18:27 +0800

    add radau

--- torchdiffeq/_impl/rk_common.py
+++ torchdiffeq/_impl/rk_common.py
@@ -281,6 +281,8 @@ class RKAdaptiveStepsizeODESolver(AdaptiveStepsizeEventODESolver):
         ########################################################
         #                      Assertions                      #
         ########################################################
+        if (dt <= 0).any():
+            print('dt < 0')
         assert t0 + dt > t0, 'underflow in dt {}'.format(dt.item())
         assert torch.isfinite(y0).all(), 'non-finite values in state `y`: {}'.format(y0)
 
